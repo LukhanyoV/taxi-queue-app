c02d98d4b7bded5699ab00b8f9fab05de44b49d5
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-05-30 12:13:36 +0200

    unit test and implementing factory function on dom complete

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -1,11 +1,51 @@
-// write your DOM code here.
-
-
 // DOM element references
+const passengersCount = document.querySelector(".passenger_queue_count")
+const taxisCount = document.querySelector(".taxi_queue_count")
+const joinQueue = document.querySelector(".join_queue")
+const leaveQueue = document.querySelector(".leave_queue")
+const joinTaxiQueue = document.querySelector(".join_taxi_queue")
+const departBtn = document.querySelector(".depart")
 
 // create Factory Function instance
-
 const taxiQueue = TaxiQueue();
 
-// DOM events
+// write your DOM code here.
+//
+taxisCount.innerHTML = taxiQueue.taxiQueueLength()
+//
+passengersCount.innerHTML = taxiQueue.queueLength()
 
+// DOM events
+//
+joinQueue.addEventListener("click", function () {
+    //
+    taxiQueue.joinQueue()
+    //
+    passengersCount.innerHTML = taxiQueue.queueLength()
+})
+
+//
+leaveQueue.addEventListener("click", function () {
+    //
+    taxiQueue.leaveQueue()
+    //
+    passengersCount.innerHTML = taxiQueue.queueLength()
+})
+
+//
+joinTaxiQueue.addEventListener("click", function () {
+    //
+    taxiQueue.joinTaxiQueue()
+    //
+    taxisCount.innerHTML = taxiQueue.taxiQueueLength()
+})
+
+//
+departBtn.addEventListener("click", function () {
+    //
+    taxiQueue.taxiDepart()
+    //
+    taxisCount.innerHTML = taxiQueue.taxiQueueLength()
+    //
+    passengersCount.innerHTML = taxiQueue.queueLength()
+})
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,28 +1,34 @@
 function TaxiQueue() {
-
+	let people = 0
+	let taxis = 0
 
 	function joinQueue() {
-
+		people++
 	}
 
 	function leaveQueue() {
-
+		if(people>0){
+			people--
+		}
 	}
 
 	function joinTaxiQueue() {
-
+		taxis++
 	}
 
 	function queueLength() {
-
+		return people
 	}
 
 	function taxiQueueLength() {
-
+		return taxis
 	}
 
 	function taxiDepart(){
-
+		if(queueLength()>=12 && taxiQueueLength() >= 1){
+			people = people - 12
+			taxis--
+		}
 	}
 
 	return {
