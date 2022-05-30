575df77860cb954b83820e3a90251b59866ba89a
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-05-30 12:45:39 +0200

    added local storage and made it work with my factory function

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -1,3 +1,24 @@
+// write your DOM code here.
+// this function will keep track of the counters in local storage
+function addToLocalStorage(key){
+    if(key === "taxi"){
+        localStorage.setItem("taxiCounter", taxiQueue.taxiQueueLength())
+    } else if(key === "people"){
+        localStorage.setItem("peopleCounter", taxiQueue.queueLength())
+    }
+}
+
+// this function will get the current counter from local storage
+function getFromLocalStorage(key){
+    if(key === "taxi"){
+        // local storage store strings so convert back to integer
+        return localStorage.getItem("taxiCounter")-""
+    } else if(key === "people"){
+        // local storage store strings so convert back to integer
+        return localStorage.getItem("peopleCounter")-""
+    }
+}
+
 // DOM element references
 const passengersCount = document.querySelector(".passenger_queue_count")
 const taxisCount = document.querySelector(".taxi_queue_count")
@@ -7,9 +28,9 @@ const joinTaxiQueue = document.querySelector(".join_taxi_queue")
 const departBtn = document.querySelector(".depart")
 
 // create Factory Function instance
-const taxiQueue = TaxiQueue();
+// get the current values from local storage
+const taxiQueue = TaxiQueue(getFromLocalStorage("taxi"), getFromLocalStorage("people"));
 
-// write your DOM code here.
 // when the page loads display the current values
 // display current taxi count
 taxisCount.innerHTML = taxiQueue.taxiQueueLength()
@@ -23,6 +44,8 @@ joinQueue.addEventListener("click", function () {
     taxiQueue.joinQueue()
     // display the number of people in the queue
     passengersCount.innerHTML = taxiQueue.queueLength()
+    // update the current counter to localstorage
+    addToLocalStorage("people")
 })
 
 // listen for when the leave queue button is clicked
@@ -31,6 +54,8 @@ leaveQueue.addEventListener("click", function () {
     taxiQueue.leaveQueue()
     // display the number of people in the queue
     passengersCount.innerHTML = taxiQueue.queueLength()
+    // update the current counter to localstorage
+    addToLocalStorage("people")
 })
 
 // listen for when the join taxi queue button is clicked
@@ -39,9 +64,11 @@ joinTaxiQueue.addEventListener("click", function () {
     taxiQueue.joinTaxiQueue()
     // show the number of taxis that are in the queue
     taxisCount.innerHTML = taxiQueue.taxiQueueLength()
+    // update the current counter to localstorage
+    addToLocalStorage("taxi")
 })
 
-// listene for when the depart button is pressed
+// listen for when the depart button is pressed
 departBtn.addEventListener("click", function () {
     // this function will check for the number of people available
     // and also it will chehck for the number of taxis available
@@ -51,4 +78,7 @@ departBtn.addEventListener("click", function () {
     taxisCount.innerHTML = taxiQueue.taxiQueueLength()
     // show the number of passangers after depature
     passengersCount.innerHTML = taxiQueue.queueLength()
+    // update the local storage after depature
+    addToLocalStorage("taxi")
+    addToLocalStorage("people")
 })
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,32 +1,43 @@
-function TaxiQueue() {
-	let people = 0
-	let taxis = 0
+function TaxiQueue(taxiCounter, peopleCounter) {
+	// just in case there are counters saved on localstorage
+	let people = peopleCounter || 0
+	let taxis = taxiCounter || 0
 
+	// increase the number of people when someone join a queue
 	function joinQueue() {
 		people++
 	}
 
+	// decrease the number of people when someone leave a queue
 	function leaveQueue() {
 		if(people>0){
 			people--
 		}
 	}
 
+	// increase the number of taxis in the queue
 	function joinTaxiQueue() {
 		taxis++
 	}
 
+	// return the number of people in the queue
 	function queueLength() {
 		return people
 	}
 
+	// return the number of taxis available
 	function taxiQueueLength() {
 		return taxis
 	}
 
 	function taxiDepart(){
+		// if there are more than or 12 people in the queue
+		// and there is also at least 1 taxi
+		// then the taxi can depature
 		if(queueLength()>=12 && taxiQueueLength() >= 1){
+			// a taxi has to leave with 12 people
 			people = people - 12
+			// only one taxi can leave at a time
 			taxis--
 		}
 	}
